1ca1c3f10581676d1fa3ba2941f722cd2efd4cbd
Punit Goyal <punitgoyal106@gmail.com>
2024-05-07 08:07:55 +0530

    search functionality in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const Navbar = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-light navbar-light fixed-top">
+      <nav className="navbar navbar-expand-lg bg-light navbar-light fixed-top" style={{height: '62px'}}>
         <div className="container-fluid mx-auto">
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <a href="/"><img
+              src="https://t4.ftcdn.net/jpg/05/13/72/29/360_F_513722905_SgxiGdjQZsdvP4ODmERsQGgW2bUwj1lT.jpg"
+              alt="JobLogo "
+              height="50px"
+              style={{ cursor: 'pointer' }}
+            /></a>
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link" aria-current="page" href="/">
@@ -37,12 +43,12 @@ const Navbar = () => {
             </ul>
             <form className="d-flex">
               <input
-                className="form-control me-2"
+                className="form-control me-2 w-32"
                 type="search"
                 placeholder="Search Job"
                 aria-label="Search"
               />
-              <button className="btn btn-outline-success" type="submit">
+              <button className="btn btn-outline-primary" type="submit">
                 Search
               </button>
             </form>
@@ -58,7 +64,10 @@ const Navbar = () => {
                 >
                   Profile
                 </a>
-                <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                <ul
+                  className="dropdown-menu dropdown-menu-end"
+                  aria-labelledby="navbarDropdown"
+                >
                   <li>
                     <a className="dropdown-item" href="/your-profile">
                       Your Profile
