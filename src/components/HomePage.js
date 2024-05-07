f394956c1ccd0040326aff3aecc41b68c2b86a93
Punit Goyal <punitgoyal106@gmail.com>
2024-05-07 08:07:18 +0530

    home page for the application

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <div style={{ padding: "100px", fontFamily: "cursive" }}>
+      <h1
+        className="text-center hover-underline-animation"
+        style={{
+          position: "relative",
+          cursor: "pointer",
+        }}
+        onMouseEnter={(e) => (e.target.style.textDecoration = 'underline')}
+        onMouseLeave={(e) => (e.target.style.textDecoration = 'none')}
+      >
+        Build Your Career In Your Dream Company
+      </h1>
+    </div>
+  );
 }
