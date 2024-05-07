7ef4db2020b10f142e06c6b2ad432eeab6d98aa4
Punit Goyal <punitgoyal106@gmail.com>
2024-05-07 21:49:28 +0530

    created routes for components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { Footer } from "./components/Footer";
 import HomePage from "./components/HomePage";
+import CandidateDashboard from "./components/CandidateDashboard";
+import JobList from "./components/JobList.js";
+import JobDetails from "./components/JobDetails";
+import './App.css';
+
 function App() {
   return (
     <>
       <div className="App">
-        <Navbar />
-        <HomePage />
+        <Router>
+          <Navbar />
+          <Route exact path="/" component={HomePage} />
+          <Route path="/candidatedashboard" component={CandidateDashboard} />
+          <Route path="/joblist" component={JobList} />
+          <Route path="/jobdetail" component={JobDetails} />
+        </Router>
         <Footer />
       </div>
     </>
